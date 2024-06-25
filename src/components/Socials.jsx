9686871a01ce2bd50720b24a1a130e8b69809e37
AndresRufus <166900128+AndresRufus@users.noreply.github.com>
2024-06-25 19:26:40 -0400

    X

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,7 +6,7 @@ import telegram from "../assets/telegram.svg";
 const Socials = () => {
   return (
     <div className="flex gap-3 justify-center">
-      <a href="https://x.com/realrufustoken?s=21&t=cdebtaSQaGp798uNWEyBjQ" target='_blank'>
+      <a href="https://x.com/realrufusdog" target='_blank'>
         <div className="rounded-lg bg-blue-300 p-2 w-fit hover:scale-110">
           <img src={twitter} alt="Twitter" className="w-6 h-6" />
         </div>
